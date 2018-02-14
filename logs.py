8d7d535abff82d0414fafcb1d0016fe32188970d
AByteCoder <abytecoder@gmail.com>
2018-02-14 13:29:04 +0530

    Modified logs.py for PEP8 standard

--- logs.py
+++ logs.py
@@ -6,40 +6,44 @@ import datetime
 
 database = "news"
 
-def find_most_popular_authors(cursor) :
+
+def find_most_popular_authors(cursor):
     """ Returns a list containing most popular authors"""
     sql_query = "SELECT name,count(path) AS views FROM authors,articles, log"\
-    +" WHERE path = CONCAT('/article/',slug) AND authors.id = author"\
-    +" GROUP BY authors.id ORDER BY views DESC"
+        + " WHERE path = CONCAT('/article/',slug) AND authors.id = author"\
+        + " GROUP BY authors.id ORDER BY views DESC"
     cursor.execute(sql_query)
     return cursor.fetchall()
+
 
 def find_most_popular_articles(cursor):
     """ Returns a list containing most popular artilces"""
     sql_query = "SELECT title,count(path) AS views FROM articles, log"\
-    +" WHERE path = CONCAT('/article/',slug)"\
-    +" GROUP BY articles.id ORDER BY views DESC LIMIT 3"
+        + " WHERE path = CONCAT('/article/',slug)"\
+        + " GROUP BY articles.id ORDER BY views DESC LIMIT 3"
     cursor.execute(sql_query)
     return cursor.fetchall()
+
 
 def find_error(cursor):
     """ Returns a List containing days with 404 error greater than 1%"""
     view_success = "CREATE OR REPLACE VIEW LOG_SUCCESS AS SELECT DATE(time) "\
-    +"AS day, COUNT(*) AS num_total FROM log GROUP BY DATE(time)"
+        + "AS day, COUNT(*) AS num_total FROM log GROUP BY DATE(time)"
     view_failure = "CREATE OR REPLACE VIEW LOG_FAILED AS SELECT DATE(time) "\
-    +"AS day, COUNT(*) AS num_failed FROM log WHERE status = '404 NOT FOUND'"\
-    +" GROUP BY DATE(time)"
-    sql_query = "SELECT DATE(time) AS day,num_failed::float/num_total * 100 AS"\
-    +" failure_percent FROM log,LOG_SUCCESS,LOG_FAILED"\
-    +" WHERE DATE(time) = LOG_SUCCESS.day AND DATE(time) = LOG_FAILED.day"\
-    +" AND (num_failed::float/num_total * 100) >= 1"\
-    +" GROUP BY DATE(time),num_failed,num_total"
+        + "AS day, COUNT(*) AS num_failed FROM log "\
+        + "WHERE status = '404 NOT FOUND' GROUP BY DATE(time)"
+    sql_query = "SELECT DATE(time) AS day,num_failed::float/num_total * 100 "\
+        + "AS failure_percent FROM log,LOG_SUCCESS,LOG_FAILED"\
+        + " WHERE DATE(time) = LOG_SUCCESS.day AND DATE(time) = "\
+        + "LOG_FAILED.day AND (num_failed::float/num_total * 100) >= 1"\
+        + " GROUP BY DATE(time),num_failed,num_total"
     cursor.execute(view_success)
     cursor.execute(view_failure)
     cursor.execute(sql_query)
     return cursor.fetchall()
 
-def main() :
+
+def main():
     """Main program which displays output for Log Analysis Project"""
     connection = psycopg2.connect(database=database)
     cursor = connection.cursor()
@@ -48,10 +52,10 @@ def main() :
     question_1 = "1. What are the most popular three articles of all time?"
     print(question_1)
     print("")
-    print("%-35s %-10s" %("Article Title","Views"))
+    print("% - 35s % - 10s" % ("Article Title", "Views"))
     print("=================================== =========")
     for article in most_articles:
-        print("%-35s %-10d" %(article[0],article[1]))
+        print("% - 35s % - 10d" % (article[0], article[1]))
     print("")
     print("--------------------------------------")
     # QUESTION: Solution for question_2
@@ -60,24 +64,26 @@ def main() :
     print("")
     print(question_2)
     print("")
-    print("%-25s %-10s" %("Author name","Views"))
+    print("% - 25s % - 10s" % ("Author name", "Views"))
     print("======================== =========")
     for author in most_popular:
-        print("%-25s %-10d" %(author[0],author[1]))
+        print("% - 25s % - 10d" % (author[0], author[1]))
     print(" ")
     print("--------------------------------------")
     # QUESTION: Solution for question_3
-    question_3 = "3. On which days did more than 1% of requests lead to errors?"
+    question_3 = "3. On which days did more than 1% of requests lead to "\
+        + "errors?"
     print("")
     print(question_3)
     print("")
     errors = find_error(cursor)
-    print("%-20s %-10s" %("Date","Error Percent"))
+    print("% - 20s % - 10s" % ("Date", "Error Percent"))
     print("===================  =========")
     for error in errors:
-        print("%-20s %-3.1f%%" %(error[0].strftime('%B %-d, %Y'),error[1]))
-
+        print("% - 20s % - 3.1f%%" % (error[0].strftime('%B %-d, %Y'),
+                                      error[1]))
     connection.close()
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     main()
