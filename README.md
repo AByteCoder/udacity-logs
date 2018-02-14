bfcd9060f0733eac346ca4e9a15b061b0da6a648
AByteCoder <abytecoder@gmail.com>
2018-02-14 13:01:26 +0530

    Solved Question 3 and Updated README

--- README.md
+++ README.md
@@ -13,3 +13,44 @@ Requirements
 psql -d news -f newsdata.sql``` command. (Note newsdata.sql can be found in this repo.)
 
 Run the script using ``` python3 logs.py ```.
+
+Note for the 3rd problem some SQL views are created
+They are.
+1. ``` CREATE VIEW LOG_SUCCESS AS SELECT DATE(time) AS day, COUNT(*) AS num_total FROM log GROUP BY DATE(time);```
+1. ``` CREATE VIEW LOG_FAILED AS SELECT DATE(time) AS day, COUNT(*) AS num_failed FROM log WHERE status = '404 NOT FOUND' GROUP BY DATE(time);```
+
+
+* **LOG_SUCCESS** contains total requests for each day.
+* **LOG_FAILED** contains all requests failed for each day.
+
+### Output
+
+```
+1. What are the most popular three articles of all time?
+
+Article Title                       Views     
+=================================== =========
+Candidate is jerk, alleges rival    338647    
+Bears love berries, alleges bear    253801    
+Bad things gone, say good people    170098    
+
+--------------------------------------
+
+2. Who are the most popular article authors of all time?
+
+Author name               Views     
+======================== =========
+Ursula La Multa           507594    
+Rudolf von Treppenwitz    423457    
+Anonymous Contributor     170098    
+Markoff Chaney            84557     
+
+--------------------------------------
+
+3. On which days did more than 1% of requests lead to errors?
+
+Date                 Error Percent
+===================  =========
+July 17, 2016        2.3%
+
+```
--- logs.py
+++ logs.py
@@ -2,6 +2,7 @@
 
 # importing database interface library.
 import psycopg2
+import datetime
 
 database = "news"
 
@@ -21,10 +22,29 @@ def find_most_popular_articles(cursor):
     cursor.execute(sql_query)
     return cursor.fetchall()
 
+def find_error(cursor):
+    """ Returns a List containing days with 404 error greater than 1%"""
+    view_success = "CREATE OR REPLACE VIEW LOG_SUCCESS AS SELECT DATE(time) "\
+    +"AS day, COUNT(*) AS num_total FROM log GROUP BY DATE(time)"
+    view_failure = "CREATE OR REPLACE VIEW LOG_FAILED AS SELECT DATE(time) "\
+    +"AS day, COUNT(*) AS num_failed FROM log WHERE status = '404 NOT FOUND'"\
+    +" GROUP BY DATE(time)"
+    sql_query = "SELECT DATE(time) AS day,num_failed::float/num_total * 100 AS"\
+    +" failure_percent FROM log,LOG_SUCCESS,LOG_FAILED"\
+    +" WHERE DATE(time) = LOG_SUCCESS.day AND DATE(time) = LOG_FAILED.day"\
+    +" AND (num_failed::float/num_total * 100) >= 1"\
+    +" GROUP BY DATE(time),num_failed,num_total"
+    cursor.execute(view_success)
+    cursor.execute(view_failure)
+    cursor.execute(sql_query)
+    return cursor.fetchall()
+
 def main() :
+    """Main program which displays output for Log Analysis Project"""
     connection = psycopg2.connect(database=database)
     cursor = connection.cursor()
     most_articles = find_most_popular_articles(cursor)
+    # QUESTION:  Solving for question_1
     question_1 = "1. What are the most popular three articles of all time?"
     print(question_1)
     print("")
@@ -34,6 +54,7 @@ def main() :
         print("%-35s %-10d" %(article[0],article[1]))
     print("")
     print("--------------------------------------")
+    # QUESTION: Solution for question_2
     most_popular = find_most_popular_authors(cursor)
     question_2 = "2. Who are the most popular article authors of all time?"
     print("")
@@ -43,6 +64,19 @@ def main() :
     print("======================== =========")
     for author in most_popular:
         print("%-25s %-10d" %(author[0],author[1]))
+    print(" ")
+    print("--------------------------------------")
+    # QUESTION: Solution for question_3
+    question_3 = "3. On which days did more than 1% of requests lead to errors?"
+    print("")
+    print(question_3)
+    print("")
+    errors = find_error(cursor)
+    print("%-20s %-10s" %("Date","Error Percent"))
+    print("===================  =========")
+    for error in errors:
+        print("%-20s %-3.1f%%" %(error[0].strftime('%B %-d, %Y'),error[1]))
+
     connection.close()
 
 if __name__ == "__main__" :
